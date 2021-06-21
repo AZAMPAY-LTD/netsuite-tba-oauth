6d784f0e1c0cd1e5cca31d39aabf942d0630817b
Hussein Mkwizu <hussein.mkwizu@gmail.com>
2021-06-21 22:38:07 +0300

    exposed algorithm and signatureMethod on NetSuiteOAuth constructor

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-'use strict';
+"use strict";
 
 // Dependencies
-const request = require('request');
-const OAuth   = require('oauth-1.0a');
-const crypto  = require('crypto');
+const request = require("request");
+const OAuth = require("oauth-1.0a");
+const crypto = require("crypto");
 
 module.exports = NetSuiteOAuth;
 
@@ -17,108 +17,128 @@ module.exports = NetSuiteOAuth;
  * @param tokenId
  * @param tokenSecret
  * @param account
+ * @param algorithm
+ * @param signatureMethod
  * @returns {PromiseLike<ArrayBuffer>}
  * @constructor
  */
-function NetSuiteOAuth(url, method, consumerKey, consumerSecret, tokenId, tokenSecret, account) {
-    this.oauth = OAuth({
-        consumer: {
-            key: consumerKey,
-            secret: consumerSecret
-        },
-        realm: account,
-        signature_method: 'HMAC-SHA1',
-        hash_function(base_string, key) {
-            return crypto.createHmac('sha1', key).update(base_string).digest('base64');
-        }
-    });
+function NetSuiteOAuth(
+  url,
+  method,
+  consumerKey,
+  consumerSecret,
+  tokenId,
+  tokenSecret,
+  account,
+  algorithm = "sha1",
+  signatureMethod = "HMAC-SHA1"
+) {
+  this.oauth = OAuth({
+    consumer: {
+      key: consumerKey,
+      secret: consumerSecret,
+    },
+    realm: account,
+    signature_method: signatureMethod,
+    hash_function(base_string, key) {
+      return crypto
+        .createHmac(algorithm, key)
+        .update(base_string)
+        .digest("base64");
+    },
+  });
 
-    this.request_data = {
-        url: url,
-        method: method
-    };
+  this.request_data = {
+    url: url,
+    method: method,
+  };
 
-    this.token = {
-        key: tokenId,
-        secret: tokenSecret
-    };
+  this.token = {
+    key: tokenId,
+    secret: tokenSecret,
+  };
 
-    this.headers = this.oauth.toHeader(this.oauth.authorize(this.request_data, this.token));
-    this.headers['Content-Type'] = 'application/json';
+  this.headers = this.oauth.toHeader(
+    this.oauth.authorize(this.request_data, this.token)
+  );
+  this.headers["Content-Type"] = "application/json";
 }
 
-NetSuiteOAuth.prototype.get = function() {
-    return new Promise((resolve, reject) => {
-        request({
-            url: this.request_data.url,
-            method: this.request_data.method,
-            headers: this.headers
-        }, function(error, response, body) {
-            if (error || response.statusCode.toString()[0] != 2) {
-                console.log('Body data:', body);
-                reject(body || error);
-            }
-            else {
-                if (typeof body == 'string') body = JSON.parse(body);
-                resolve(body || error);
-            }
-        });
-
-    });
+NetSuiteOAuth.prototype.get = function () {
+  return new Promise((resolve, reject) => {
+    request(
+      {
+        url: this.request_data.url,
+        method: this.request_data.method,
+        headers: this.headers,
+      },
+      function (error, response, body) {
+        if (error || response.statusCode.toString()[0] != 2) {
+          console.log("Body data:", body);
+          reject(body || error);
+        } else {
+          if (typeof body == "string") body = JSON.parse(body);
+          resolve(body || error);
+        }
+      }
+    );
+  });
 };
 
-NetSuiteOAuth.prototype.post = function(data) {
-    return new Promise((resolve, reject) => {
-        request({
-            url: this.request_data.url,
-            method: this.request_data.method,
-            json: data,
-            headers: this.headers
-        }, function(error, response, body) {
-            if (error || response.statusCode.toString()[0] != 2) {
-                console.log('Body data:', body);
-                reject(body || error);
+NetSuiteOAuth.prototype.post = function (data) {
+  return new Promise((resolve, reject) => {
+    request(
+      {
+        url: this.request_data.url,
+        method: this.request_data.method,
+        json: data,
+        headers: this.headers,
+      },
+      function (error, response, body) {
+        if (error || response.statusCode.toString()[0] != 2) {
+          console.log("Body data:", body);
+          reject(body || error);
+        } else {
+          if (typeof body == "string") {
+            try {
+              body = JSON.parse(body);
+            } catch (error) {
+              console.log("unable to parse response body");
+              reject(error);
             }
-            else {
-                if (typeof body == 'string') {
-                    try {
-                        body = JSON.parse(body);
-                    } catch (error){
-                        console.log('unable to parse response body');
-                        reject(error);
-                    }
-                }
-                resolve(body || error);
-            }
-        });
-
-    });
+          }
+          resolve(body || error);
+        }
+      }
+    );
+  });
 };
 
-NetSuiteOAuth.prototype.put = function(data) {
-    return new Promise((resolve, reject) => {
-        request({
-            url: this.request_data.url,
-            method: this.request_data.method,
-            json: data,
-            headers: this.headers
-        }, function(error, response, body) {
-            if (error || response.statusCode.toString()[0] != 2) {
-                console.log('Body data:', body);
-                reject(body || error);
+NetSuiteOAuth.prototype.put = function (data) {
+  return new Promise((resolve, reject) => {
+    request(
+      {
+        url: this.request_data.url,
+        method: this.request_data.method,
+        json: data,
+        headers: this.headers,
+      },
+      function (error, response, body) {
+        if (error || response.statusCode.toString()[0] != 2) {
+          console.log("Body data:", body);
+          reject(body || error);
+        } else {
+          if (typeof body == "string") {
+            try {
+              body = JSON.parse(body);
+            } catch (error) {
+              console.log("unable to parse response body");
+              reject(error);
             }
-            else {
-                if (typeof body == 'string') {
-                    try {
-                        body = JSON.parse(body);
-                    } catch (error){
-                        console.log('unable to parse response body');
-                        reject(error);
-                    }
-                }
-                resolve(body || error);
-            }
-        });
-
-    });
+          }
+          resolve(body || error);
+        }
+      }
+    );
+  });
 };
